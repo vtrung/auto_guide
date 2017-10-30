f9317fdc3600a03097c0991da95e2a037f79d927
Ving Trung <vtrung>
2017-10-30 01:15:57 -0700

    Added prototype to filter search for cars in database by year, make, model

--- src/AppBundle/Controller/AutoController.php
+++ src/AppBundle/Controller/AutoController.php
@@ -14,19 +14,154 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Utility\CarUtility;
 
+
 class AutoController Extends Controller
 {
     /**
      * @Route("/", name="auto_landing")
      */
     public function indexAction(Request $request){
-        $cars = [
-            (new Car())->setYear('2005')->setMake('Toyota')->setModel('Corolla')->setMpg(28),
-            (new Car())->setYear('2015')->setMake('Toyota')->setModel('Prius V')->setMpg(41),
-            (new Car())->setYear('2008')->setMake('Hummer')->setModel('H3')->setMpg(15),
-            (new Car())->setYear('2017')->setMake('Ford')->setModel('Mustang')->setMpg(24)
 
-        ];
+        $cu = new CarUtility();
+        $em = $this->getDoctrine()->getManager();
+        $years = $cu->GetAllYears($em);
+        var_dump($years);
+        $form = $this->createFormBuilder()
+            ->add('year', ChoiceType::class, array(
+                'choices' => $years,
+                'choice_label' => function($year){
+                    return $year;
+                }
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Next'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $year = $form->getData()['year'];
+            return $this->redirectToRoute('auto_year_landing', [
+                'year' => $year
+            ]);
+        };
+
+
+        return $this->render('auto/index.html.twig', [
+            'title' => "Auto Guide Efficiency Calculator",
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/car/{year}", name="auto_year_landing")
+     */
+    public function carYearAction(Request $request, $year){
+        $cu = new CarUtility();
+        $em = $this->getDoctrine()->getManager();
+        $makes = $cu->GetMake($em, $year);
+
+        $form = $this->createFormBuilder()
+
+            ->add('make', ChoiceType::class, array(
+                'choices' => $makes,
+                'choice_label' => function($make){
+                    return $make;
+                }
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Next'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $make = $form->getData()['make'];
+            return $this->redirectToRoute('auto_make_landing', [
+                'year' => $year,
+                'make' => $make
+            ]);
+        };
+
+
+        return $this->render('auto/index.html.twig', [
+            'year' => $year,
+            'title' => "Auto Guide Efficiency Calculator",
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/car/{year}/{make}", name="auto_make_landing")
+     */
+    public function carMakeAction(Request $request, $year, $make){
+        $cu = new CarUtility();
+        $em = $this->getDoctrine()->getManager();
+        $models = $cu->GetModel($em, $year, $make);
+
+        $form = $this->createFormBuilder()
+
+            ->add('model', ChoiceType::class, array(
+                'choices' => $models,
+                'choice_label' => function($model){
+                    /** @var Car $model */
+                    return $model->getName();
+                }
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Next'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Car $model */
+            $model = $form->getData()['model'];
+            return $this->redirectToRoute('auto_car_landing', [
+                'id' => $model->getId()
+            ]);
+        };
+
+
+        return $this->render('auto/index.html.twig', [
+            'year' => $year,
+            'title' => "Auto Guide Efficiency Calculator",
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/carindex/{id}", name="auto_car_landing")
+     */
+    public function carAction(Request $request, $id){
+        /** @var Car $model */
+        $car = $this->getDoctrine()
+            ->getRepository(Car::class)
+            ->findById($id)[0];
+
+        var_dump($car);
+
+        return $this->render('auto/carindex.html.twig', [
+            'car' => $car,
+            'title' => $car->getName()
+        ]);
+    }
+
+    /**
+     * @Route("/compare", name="auto_compare_landing")
+     */
+    public function compareAction(Request $request){
+//        $cars = [
+//            (new Car())->setYear('2005')->setMake('Toyota')->setModel('Corolla')->setMpg(28),
+//            (new Car())->setYear('2015')->setMake('Toyota')->setModel('Prius V')->setMpg(41),
+//            (new Car())->setYear('2008')->setMake('Hummer')->setModel('H3')->setMpg(15),
+//            (new Car())->setYear('2017')->setMake('Ford')->setModel('Mustang')->setMpg(24)
+//        ];
+
+
+        $cars = $this->getDoctrine()
+            ->getRepository(Car::class)
+            ->findAll();
+
 
         $compare = [];
         $compare_mpg = 0;
@@ -61,7 +196,7 @@ class AutoController Extends Controller
         }
 
 
-        return $this->render('auto/index.html.twig', [
+        return $this->render('auto/compare.html.twig', [
             'title' => "Auto Guide Efficiency Calculator",
             'cars' => $cars,
             'form' => $form->createView(),
@@ -69,7 +204,32 @@ class AutoController Extends Controller
             'comparempg' => $compare_mpg
         ]);
     }
-
-
+//
+//    /**
+//     * @Route("/auto/api/year", name="auto_api_year")
+//     * @Method("GET")
+//     */
+//    public function apiYearAction(){
+//        $cu = new CarUtility();
+//        return new Response(json_encode($cu->GetAllYears()));
+//    }
+//
+//    /**
+//     * @Route("/auto/api/{year}", name="auto_api_year")
+//     * @Method("GET")
+//     */
+//    public function apiMakeAction($year){
+//        $cu = new CarUtility();
+//        return new Response(json_encode($cu->GetMake($year)));
+//    }
+//
+//    /**
+//     * @Route("/auto/api/{year}/{make}", name="auto_api_year")
+//     * @Method("GET")
+//     */
+//    public function apiModelAction($year, $make){
+//        $cu = new CarUtility();
+//        return new Response(json_encode($cu->GetModel($year, $make)));
+//    }
 
 }
--- src/AppBundle/Utility/CarUtility.php
+++ src/AppBundle/Utility/CarUtility.php
@@ -16,12 +16,40 @@ class CarUtility
 
         $mpg = 0;
 
-        dump($car1);
-        dump($car2);
-
         $mpg = $car1->getMpg() - $car2->getMpg();
 
         return $mpg;
 
     }
+
+
+    public function GetAllYears($em){
+        $query = $em->createQuery(
+            'SELECT DISTINCT c.year FROM AppBundle:Car c'
+        );
+        return $query->getResult();
+    }
+
+    public function GetMake($em, $year){
+        $query = $em->createQuery(
+            'SELECT DISTINCT c.make 
+              FROM AppBundle:Car c 
+              WHERE c.year=:year'
+        )->setParameter('year', $year);
+
+        return $query->getResult();
+    }
+
+    public function GetModel($em, $year, $make){
+        $query = $em->createQuery(
+            'SELECT DISTINCT c FROM AppBundle:Car c WHERE  c.year=:year AND c.make=:make'
+        )
+            ->setParameter('year',$year)
+            ->setParameter('make',$make);
+
+        return $query->getResult();
+    }
+
+
+
 }
